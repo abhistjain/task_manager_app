b6f1bba4dfcaccf8607503338dfff32df7c98413
Abhist jain <abhistjain@gmail.com>
2024-07-27 11:00:44 +0530

    index.js

--- index.js
+++ index.js
@@ -27,3 +27,8 @@ app.use('/api', analyticsRoutes); // Analytics route
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get( '/' , (req,res) => {
+    req.send("<h1>This is our homepage</h1>");
+});
